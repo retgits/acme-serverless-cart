aa4ac914c4d6bfa1361c9500b5557cf600ec9e2e
retgits <lstigter@gmail.com>
2020-03-11 17:14:18 -0700

    feat: add pulumi deployment

--- internal/datastore/api.go
+++ internal/datastore/api.go
@@ -1,8 +1,14 @@
+// Package datastore contains the interfaces that the Cart service
+// in the ACME Serverless Fitness Shop needs to store and retrieve data.
+// In order to add a new service, the Manager interface
+// needs to be implemented.
 package datastore
 
 import cart "github.com/retgits/acme-serverless-cart"
 
-// Manager ...
+// Manager is the interface that describes the methods the
+// data store needs to implement to be able to work with
+// the ACME Serverless Fitness Shop.
 type Manager interface {
 	GetItems(userID string) (cart.Items, error)
 	AddItem(userID string, i cart.Item) error
